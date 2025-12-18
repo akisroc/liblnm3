60cecbfa1a52b3867e6deb7c76589d9e21d6d231
Adrien-H <adrien.h@tuta.io>
2025-12-18 11:11:06 +0100

    Add docs

--- mix.exs
+++ mix.exs
@@ -4,10 +4,16 @@ defmodule Liblnm3.MixProject do
   def project do
     [
       app: :liblnm3,
+      name: "liblnm3",
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/akisroc/liblnm3",
+      docs: [
+        main: "akisroc/liblnm3",
+        extras: ["README.md", "LICENSE"]
+      ]
     ]
   end
 
@@ -21,6 +27,14 @@ defmodule Liblnm3.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {
+        :ex_doc,
+        "~> 0.34",
+        only: :dev,
+        runtime: false,
+        warn_if_outdated: true,
+        extras: ["README.md", "LICENSE"]
+      }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
