6c6596b12cf2f4704f7b2cb712d136dca4cb9117
Adrien-H <adrien.h@tuta.io>
2026-01-06 11:26:40 +0100

    Fix authentication
    Was sending binary token instead of base64

--- services/platform/lib/platform_web/controllers/session_controller.ex
+++ services/platform/lib/platform_web/controllers/session_controller.ex
@@ -5,6 +5,7 @@ defmodule PlatformWeb.SessionController do
     case Platform.Accounts.authenticate_user(email, password) do
       {:ok, user} ->
         token = Platform.Accounts.Session.generate_session_token(user)
+        |> Base.url_encode64(padding: false);
 
         conn
         |> put_resp_cookie("_platform_user_token", token,
@@ -13,6 +14,13 @@ defmodule PlatformWeb.SessionController do
           same_site: "Lax",
           max_age: 60 * 60 * 24 * 120 # 120 days
         )
+        |> put_status(:ok)
+        |> json(%{
+          token: token,
+          user_id: user.id,
+          user_email: user.email,
+          user_slug: user.slug
+        })
 
       {:error, _reason} ->
         conn
